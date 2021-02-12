a4c5fead6979968483185af1153e5b4730ca702d
lisaau <lisaau@users.noreply.github.com>
2021-02-12 15:50:03 -0800

    Add interactivity for image resize

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
     @tracked isLarge = false;
